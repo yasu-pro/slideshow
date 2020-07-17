6316407778920178f4783a9888ff2052245fd969
yasu-pro <cb1-y.s@docomo.ne.jp>
2020-07-17 22:26:18 +0900

    スライド成功＋

--- index.js
+++ index.js
@@ -12,13 +12,6 @@ const picArray = [pic1, pic2, pic3, pic4];
 //画像の配置設定
 picArray[0].style.left = "-10%";
 picArray[0].style.transform = "translate(10%)";
-// let cilentRect = pic1.getBoundingClientRect();
-
-// let x = cilentRect.left;
-// let gx = window.pageXOffset + x;
-
-// console.log(gx);
-
 picArray[1].style.left = "50%";
 picArray[1].style.transform = "translate(-50%)";
 picArray[2].style.left = "110%";
@@ -68,75 +61,73 @@ function decrement4() {
 //-------写真1枚目処理----------------------------------------------------
 function slideScreen(a) {
   picArray[0].style.left = a + "px";
-  console.log(a, picArray[0].style.left);
-  if (result < px - afterSlide * 1.1) {
-    let spliceResult = picArray.splice(0, 1);
-    picArray.push(spliceResult);
-    console.log(picArray[0]);
-    console.log(picArray[1]);
-    console.log(picArray[2]);
-    console.log(picArray[3]);
-  }
+  // console.log(picArray[0].style.left);
 }
+//   if (result < ) {
+//     let spliceResult = picArray.splice(0, 1);
+//     picArray.push(spliceResult);
+//     console.log(picArray);
+//   }
+// }
 
 //-------写真2枚目処理----------------------------------------------------
 function slideScreen2(b) {
-  picArray[0].style.left = b + "px";
-  if (result < px - afterSlide * 1.1) {
-    let spliceResult = picArray.splice(0, 1);
-    picArray.push(spliceResult);
+  picArray[1].style.left = b + "px";
+
+  if (result2 < px + afterSlide * 0.6) {
   }
 }
 
-//-------写真3枚目処理----------------------------------------------------
+// -------写真3枚目処理----------------------------------------------------;
 function slideScreen3(c) {
-  picArray[0].style.left = c + "px";
-  if (result < px - afterSlide * 1.1) {
-    let spliceResult = picArray.splice(0, 1);
-    picArray.push(spliceResult);
+  picArray[2].style.left = c + "px";
+  if (result2 < px + afterSlide * 0.6) {
   }
 }
 
-//-------写真4枚目処理----------------------------------------------------
+// -------写真4枚目処理----------------------------------------------------;
 function slideScreen4(d) {
-  picArray[0].style.left = d + "px";
-  if (result < px - afterSlide * 1.1) {
-    let spliceResult = picArray.splice(0, 1);
-    picArray.push(spliceResult);
+  picArray[3].style.left = d + "px";
+  if (result2 < px + afterSlide * 0.6) {
   }
 }
 
 //-------イベント処理----------------------------------------------------
 
 let px = "";
+let x = "";
 let px2 = "";
+let x2 = "";
 let px3 = "";
+let x3 = "";
 let px4 = "";
+let x4 = "";
 btnNext.addEventListener("click", (e) => {
   e.preventDefault();
   //要素の位置座標を取得
   let cilentRect = picArray[0].getBoundingClientRect();
-  let x = cilentRect.left;
+  x = window.pageXOffset + cilentRect.left;
   //keft-10%で画像の位置をー10％している。”window.pageXOffset + x”のみの場合基準は0（left:0px）からになるので、切れている分の長さをを引く（- afterSlide * 0.1）と実際にある画像（切れている分の）の位置が出る。
-  px = window.pageXOffset + x - afterSlide * 0.1;
+  px = x - afterSlide * 0.1;
 
   //要素の位置座標を取得
   let cilentRect2 = picArray[1].getBoundingClientRect();
-  let x2 = cilentRect2.left;
+  x2 = window.pageXOffset + cilentRect2.left;
   //- afterSlide * 0.5を入れている理由は、left50%からtranslateで画像の中央を基準にしているため。
-  px2 = window.pageXOffset + x2 + afterSlide * 0.5;
+  px2 = x2 + afterSlide * 0.5;
+  console.log(px);
 
   //要素の位置座標を取得
   let cilentRect3 = picArray[2].getBoundingClientRect();
-  let x3 = cilentRect3.left;
+  x3 = window.pageXOffset + cilentRect3.left;
   //- afterSlide * 0.5を入れている理由は、left50%からtranslateで画像の中央を基準にしているため。
-  px3 = window.pageXOffset + x3 + afterSlide * 1.1;
+  px3 = x3 + afterSlide * 1.1;
 
   //要素の位置座標を取得
   let cilentRect4 = picArray[3].getBoundingClientRect();
-  let x4 = cilentRect4.left;
+  x4 = window.pageXOffset + cilentRect4.left;
   //- afterSlide * 0.5を入れている理由は、left50%からtranslateで画像の中央を基準にしているため。
-  px4 = window.pageXOffset + x4 + afterSlide * 1.7;
+  px4 = x4 + afterSlide * 1.7;
 
   setTimer();
 });
@@ -151,13 +142,12 @@ function setTimer() {
     decrement2();
     decrement3();
     decrement4();
-    if (result < px - afterSlide * 1.1) {
+
+    //px+afterSlide*0.1でleft0
+    if (result < x - (x2 - px)) {
+      //result2 < px + afterSlide * 0.6
       //↑↑は、+afterSlide*0.1で画像の右側が0になると止まる。
       clearInterval(set);
     }
-    // setInterval解除
-    // if (result2 < px - afterSlide) {
-    //   clearInterval(set);
-    // }
   }, countTime);
 }
